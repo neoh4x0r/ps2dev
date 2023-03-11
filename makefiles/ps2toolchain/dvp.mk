7e2b376e7b16eff32db81106911dfe575e709dac
William L. DeRieux IV <WilliamDeRieux@gmail.com>
2023-03-11 18:12:51 -0500

    + updated dvp makefile -- commands should end with a semicolon ';'

--- makefiles/ps2toolchain/dvp.mk
+++ makefiles/ps2toolchain/dvp.mk
@@ -8,17 +8,17 @@ DVP_DEPS += .ps2toolchain-$(TARGET_PS2TOOLCHAIN_DVP)-binutils
 .ps2toolchain-$(TARGET_PS2TOOLCHAIN_DVP)-binutils:
 	@$(call stage,"$(PS2TOOLCHAIN_FOLDER)/$(TARGET_PS2TOOLCHAIN_DVP)/binutils")
 	@for TARGET in $(DVP_TARGETS); do \
-		cd "$(BUILD_FOLDER)/$(PS2TOOLCHAIN_FOLDER)/$(TARGET_PS2TOOLCHAIN_DVP)/binutils" && \
-		rm -rf "build-$$TARGET" && \
-		mkdir "build-$$TARGET" && \
-		cd "build-$$TARGET" && \
+		cd "$(BUILD_FOLDER)/$(PS2TOOLCHAIN_FOLDER)/$(TARGET_PS2TOOLCHAIN_DVP)/binutils"; \
+		rm -rf "build-$$TARGET"; \
+		mkdir "build-$$TARGET"; \
+		cd "build-$$TARGET"; \
 		CFLAGS="$(am_dvp_binutils_cflags)" ../configure \
 			--quiet \
 			--prefix="$(PS2DEV)/$(TARGET_PS2TOOLCHAIN_DVP)" \
 			--target="$$TARGET" \
 			--disable-nls \
 			--disable-build-warnings \
-			$(TARG_XTRA_OPTS) && \
+			$(TARG_XTRA_OPTS); \
 		$(MAKE) --quiet -j $(NUM_JOBS) CFLAGS="$(CFLAGS) -D_FORTIFY_SOURCE=0 -O2 -Wno-implicit-function-declaration" LDFLAGS="$(LDFLAGS) -s"; \
 		$(MAKE) --quiet -j $(NUM_JOBS) install; \
 	done
