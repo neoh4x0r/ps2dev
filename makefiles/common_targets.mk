ac4106a3334b803712c7767954223738b45b6ab0
William L. DeRieux IV <WilliamDeRieux@gmail.com>
2023-03-13 15:01:48 -0400

    + added a clean-ps2dev target that will clean up the $(PS2DEV) folder + when cleaning output a message stating what is being done

--- makefiles/common_targets.mk
+++ makefiles/common_targets.mk
@@ -1,9 +1,17 @@
 
 # clean: clean only the build folder
 clean:
+	@echo "[CLEANING: $(BUILD_FOLDER)]"
 	@rm -rf "$(BUILD_FOLDER)"
 
-# reset checkout and synchronize submodules
+# dist-clean: reset checkout and synchronize submodules
 dist-clean:
+	@echo "[RESETTING submodules]"
 	@$(call ResetCheckout)
+	@echo "[SYNCING/UPDATING submodules]"
 	@$(call PrepareExternals)
+
+# clean $(PS2DEV) folder
+clean-ps2dev:
+	@echo "[CLEANING: $(PS2DEV)]"
+	@rm -rf "$(PS2DEV)"
